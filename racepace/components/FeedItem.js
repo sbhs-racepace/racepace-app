3f29e156881ac054999695e8cdac799be2edaef3
Jason Yu <jasonyu0100@gmail.com>
2019-04-15 19:25:52 +1000

    fixed feed item

--- racepace/components/FeedItem.js
+++ racepace/components/FeedItem.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Text, TextInput, View, StyleSheet, Image, TouchableOpacity } from 'react-native';
-import Button from "Button"
+import Button from "./Button"
 
 export class FeedItem extends React.Component {
 	constructor(props) {
@@ -10,21 +10,21 @@ export class FeedItem extends React.Component {
 	render() {
 		return (
 			<View>
-			<View style={{flexDirection: "row"}}>
-				<Image source='../assets/cat.jpeg' />
-				<View>
-				<Text>{this.props.username}</Text>
-				<Text>{this.props.posttime}</Text>
+				<View style={{flexDirection: "row"}}>
+					<Image source='../assets/cat.jpeg' />
+					<View>
+						<Text>{this.props.username}</Text>
+						<Text>{this.props.posttime}</Text>
+					</View>
 				</View>
-			</View>
-			<Text>{this.props.routename}</Text>
-			<Text>Description: {this.props.description}</Text>
-			<Text>Stats: {this.props.length}km {this.props.time}m</Text>
-			<Image source='../assets/cat.jpeg' />
-			<View style={{flexDirection: "row"}}>
-				<Button text="Like" />
-				<Button text="Comment" /> 
-			</View>
+				<Text>{this.props.routename}</Text>
+				<Text>Description: {this.props.description}</Text>
+				<Text>Stats: {this.props.length}km {this.props.time}m</Text>
+				<Image source='../assets/cat.jpeg' />
+				<View style={{flexDirection: "row"}}>
+					<Button text="Like"/>
+					<Button text="Comment"/>
+				</View>
 			</View>
 		)
 	}
