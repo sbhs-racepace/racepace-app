0b836c6d23a156ed045468c5db7369a6ec049361
Jason Yu <jasonyu0100@gmail.com>
2019-04-15 20:56:27 +1000

    fixed route list screen and deleted deprecated homescreen

--- racepace/screens/RouteListScreen.js
+++ racepace/screens/RouteListScreen.js
@@ -1,34 +1,51 @@
 import React from 'react';
-import { StyleSheet, View, Text, Alert } from 'react-native';
+import { StyleSheet, View, Text, Alert, ScrollView } from 'react-native';
 import Button from "../components/Button"
 import "../global.js"
 
 const styles = StyleSheet.create({
-  routeBox: {
-    borderWidth: 2
+
+  route_item: {
+    padding:"3%",
+    margin:"3%",
+    borderWidth:1,
+    borderColor:"black",
+    borderStyle:"solid",
+    borderRadius:20,
+    flexDirection:"column",
+  },
+  route_box: {
+    flexDirection:"column",
   }
 })
+
+class RouteItem extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <View style={styles.route_item}>
+        <Text>{this.props.route.start} to {this.props.route.end}</Text>
+        <Text>{this.props.route.dist}km</Text>
+      </View>
+    )
+  }
+}
+
 export default class RouteListScreen extends React.Component {
   constructor(state) {
     super(state);
   }
   
   render() {
-    let routeElements = (
-        <View style={styles.routeBox}>
-          <Text>{global.user_routes[0].begin} -> {global.user_routes[0].end}</Text>
-        </View>
-      )
-    for (let route of global.user_routes) {
-      routeElements += (
-        <View style={styles.routeBox}>
-          <Text>{route.begin} -> {route.end}</Text>
-        </View>
-      );
-    }
+    let routes = global.user_routes.map(route => <RouteItem route={route}/>);
     return (
-      <View style={{flex:1}}>
-        {routeElements}
+      <View style={{flexDirection:"column", flex:1}}>
+        <ScrollView style={styles.route_box}>
+          {routes}
+        </ScrollView>
       </View>
     );
   }
