20ad3a284065d1567ce48e868d2cca4233c7e349
Jason Yu <jasonyu0100@gmail.com>
2019-04-15 21:45:00 +1000

    import feeditem for feedscreen

--- racepace/screens/FeedScreen.js
+++ racepace/screens/FeedScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import FeedItem from "../components/FeedItem"
 
 export default class FeedScreen extends React.Component {
   render() {
