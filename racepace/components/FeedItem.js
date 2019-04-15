d7c082697aea694c35ced9a921cba6541fcf6057
sunnyyan01 <32755606+sunnyyan01@users.noreply.github.com>
2019-04-15 11:37:13 +1000

    Add FeedItem component WIP

---
+++ racepace/components/FeedItem.js
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import { Text, TextInput, View, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import Button from "Button"
+
+export class FeedItem extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+	
+	render() {
+		return (
+			<View>
+			<View style={{flexDirection: "row"}}>
+				<Image source='../assets/cat.jpeg' />
+				<View>
+				<Text>{this.props.username}</Text>
+				<Text>{this.props.posttime}</Text>
+				</View>
+			</View>
+			<Text>{this.props.routename}</Text>
+			<Text>Description: {this.props.description}</Text>
+			<Text>Stats: {this.props.length}km {this.props.time}m</Text>
+			<Image source='../assets/cat.jpeg' />
+			<View style={{flexDirection: "row"}}>
+				<Button text="Like" />
+				<Button text="Comment" /> 
+			</View>
+			</View>
+		)
+	}
+}
--- racepace/screens/DetailScreen.js
+++ racepace/screens/DetailScreen.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Prompt, display_prompt } from '../components/Prompt';
 import { View, Text, Alert } from 'react-native';
 import Button from "../components/Button"
+import {FeedItem} from "../components/FeedItem"
 //import "../global.js"
 
 export default class DetailScreen extends React.Component {
@@ -11,8 +12,13 @@ export default class DetailScreen extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Details Screen</Text>
-        <Text>Name: {this.props.navigation.state.routeName}</Text>
+        <FeedItem
+			username="Test User"
+			posttime="3 hours ago"
+			routename="Test Route"
+			description="This is a great route"
+			length="2.2"
+			time="30"
       </View>
     );
   }
