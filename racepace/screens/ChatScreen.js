f33c4b653e3a6e7695daf287054d7e443ac520cd
Jason Yu <jasonyu0100@gmail.com>
2019-04-15 18:21:38 +1000

    Update Chatscreen js with dummy data

--- racepace/screens/ChatScreen.js
+++ racepace/screens/ChatScreen.js
@@ -7,35 +7,121 @@ import {
   StyleSheet,
   Image,
   Alert,
+  Button,
 } from 'react-native';
-import Button from '../components/Button.js'
 import '../global';
 
-const STYLES = StyleSheet.create({
-  chat_input :{
-    marginTop : 5,
-    paddingLeft : 3,
-    borderWidth : 2,
-    width : "90%",
-    height : "10%",
-    right : "10%",
+const styles = StyleSheet.create({
+  chat_screen: {
+    flex: 5,
+    backgroundColor: "red",
+  },
+  message_input_box: {
+    flex: 1,
+    backgroundColor: "green",
+    flexDirection:"row",
+  },
+  message_input: {
+    fontSize: 20,
+  },
+  message_bubble: {
+    color:"white",
+    fontSize:20,
+    backgroundColor:"blue",
+    borderStyle:"solid",
+    borderColor: "red",
+    padding:"3%",
+    margin: "3%",
+    textAlign: 'center',
+  },
+  user_message: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+  },
+  other_message: {
+    flexDirection: 'row',
+  },
+  submit_button: {
+    backgroundColor:"blue",
+    color:"purple",
   }
-})
+});
+
+class MessageBubble extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  message(text) {
+    return (
+      <View style={{width:"80%"}}>
+        <Text style={styles.message_bubble}>{text}</Text>
+      </View>
+    );
+  }
+
+  userMessage(text) {
+    return (
+      <View style={styles.user_message}>
+        {this.message(text)}
+      </View>
+    );
+  }
+  
+  otherMessage(text) {
+    return (
+      <View style={styles.other_message}>
+        {this.message(text)}
+      </View>
+    );
+  }
+
+  render() {
+    if (this.props.sender == true) {
+      message = this.userMessage(this.props.text);
+    } else {
+      message = this.otherMessage(this.props.text);
+    }
+
+    return (
+      <View>
+        {message}
+      </View>
+    )
+  }
+}
 
 export default class ChatScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      messages: [
+        {text:"Hello my name is jason and i come from africa and i am black",sender:true},
+        {text:"Hello my name is jason and i come from africa and i am black",sender:false},
+        {text:"Hello my name is jason and i come from africa and i am black",sender:true},
+        {text:"Hello my name is jason and i come from africa and i am black",sender:false},
+        {text:"Hello my name is jason and i come from africa and i am black",sender:true}
+      ]
+    }
+  }
+
   render() {
     return(
-      <View>
-        <ScrollView>
-                  
-        </ScrollView>
-        <TextInput
-        style = {STYLES.chat_input}
-        placeholder = "Type a Message..."
-        />
 
-        <Button />
-      
+      <View style={{flex: 1}}>
+        <View style={styles.chat_screen}>
+          <ScrollView>
+            {this.state.messages.map(message => <MessageBubble text={message.text} sender={message.sender}/>)}
+          </ScrollView>
+        </View>
+        <View style={styles.message_input_box}>
+          <View>
+            <TextInput style={styles.message_input} placeholder="Enter a message..." editable={true}/>
+          </View>
+          <View>
+            <Button style={styles.submit_button} title="Submit Message"/>
+          </View>
+        </View>
       </View>
     )
   }
