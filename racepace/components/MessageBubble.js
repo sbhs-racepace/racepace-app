bc4e533bdd1e8b1ca6af414f869cad6b5878cbf3
Jason Yu <jasonyu0100@gmail.com>
2019-04-18 20:48:15 +1000

    Added better functionality for messages

--- racepace/components/MessageBubble.js
+++ racepace/components/MessageBubble.js
@@ -13,18 +13,15 @@ import {
 } from 'react-native';
 
 const STYLES = StyleSheet.create({
-  message_container: {
-    fontSize:20,
-    padding:"3%",
-    margin: "3%",
-    borderRadius:20,
-  },
   user_message: {
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
+    flexDirection: 'row-reverse',
   },
   other_message: {
     flexDirection: 'row',
+  },
+  message_text: {
+    fontSize:20,
+    padding:"3%",
   }
 });
 
@@ -35,15 +32,33 @@ export default class MessageBubble extends React.Component {
   }
 
   render() {
-    let message_style = this.props.sender ? STYLES.user_message : STYLES.other_message;
-    let message_color = this.props.sender ? {backgroundColor:"rgb(0, 153, 255)"} : {backgroundColor:"rgb(215,215,215)"};
-    let text = this.props.sender ? {color:"white",textAlign:"right"} : {color:"black",textAlign:"left"};
+    let message_style = (this.props.sender==true) ? STYLES.user_message : STYLES.other_message;
+    let other_style = (this.props.sender==true) ? STYLES.other_message : STYLES.user_message;
+    let message_color = (this.props.sender==true) ? {backgroundColor:"rgb(0, 153, 255)"} : {backgroundColor:"rgb(215,215,215)"};
+    let profileImage, profileName;
+    if (this.props.sameSender) {
+      profileImage = (<Image style={{width:16,height:16,borderRadius:8, margin:1}}/>);
+      profileName = null
+    } else {
+      profileImage = (<Image source={require('../assets/cat.jpeg')} style={{width:16,height:16,borderRadius:8, margin:1}}/>);
+      profileName = (
+        <View style={message_style}>
+          <Text style={{fontSize:8}}>Sender Name</Text>
+        </View>
+      )
+    }
     return (
-      <View style={message_style}>
-        <View style={[STYLES.message_container,message_color]}>
-         <Text style={text}>{this.props.text}</Text>
-       </View>
+      <View>
+        <View style={[message_style,{margin:5,alignItems:"center"}]}>
+          {profileImage}
+          <View style={{flexDirection:"column", flex:1, maxWidth:"70%"}}>
+            {profileName}
+            <View style={[message_color,{borderRadius:20}]}>
+              <Text style={STYLES.message_text}>{this.props.text}</Text>
+            </View>
+          </View>
+        </View>
       </View>
-    )
+    );
   }
 }
