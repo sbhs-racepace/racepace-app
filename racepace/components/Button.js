cf54a31dd1d3663f15059b05f82f17b24de886a3
sunnyyan01 <32755606+sunnyyan01@users.noreply.github.com>
2019-04-19 10:33:36 +1000

    Button component now displays images

--- racepace/components/Button.js
+++ racepace/components/Button.js
@@ -1,28 +1,38 @@
 import * as React from 'react';
-import { Text, View, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { Image, Text, View, StyleSheet, TouchableOpacity } from 'react-native';
 export default class Button extends React.Component {
   constructor(props) {
     super(props);
   }
 
   render() {
-    let styles = {
+    let styles = StyleSheet.create({
       button: {
-        backgroundColor: "rgb(0, 153, 255)",
+        backgroundColor: 'rgb(0, 153, 255)',
         borderWidth: 1,
       },
       text: {
-        textAlign:"center",
-        color:"white"
-      }
-    };
-    if (this.props.style) styles.button = Object.assign(styles.button,this.props.style)
-    if (this.props.text_style) styles.text = Object.assign(styles.text, this.props.text_style)
-    styles = StyleSheet.create(styles)
-    
+        textAlign: 'center',
+        color: 'white',
+      },
+      img: {},
+    });
+
     return (
-      <TouchableOpacity style={styles.button} onPress={this.props.onPress}>
-        <Text style={styles.text}>{this.props.text}</Text>
+      <TouchableOpacity
+        style={StyleSheet.flatten([styles.button, this.props.style])}
+        onPress={this.props.onPress}>
+        {this.props.img && 
+          <Image
+            source={this.props.img}
+            style={StyleSheet.flatten([styles.img, this.props.img_style])}
+          />
+        }
+        {this.props.text && 
+          <Text style={StyleSheet.flatten([styles.text, this.props.text_style])}>
+            {this.props.text}
+          </Text>
+        }
       </TouchableOpacity>
     );
   }
