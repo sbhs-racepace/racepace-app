bf88d704433f8523ff7655e5c21395230705b3b3
Jason Yu <jasonyu0100@gmail.com>
2019-04-27 13:10:30 +1000

    Added calories tracker

--- racepace/screens/RunSetupScreen.js
+++ racepace/screens/RunSetupScreen.js
@@ -1,36 +1,10 @@
 import React from 'react';
 import { StyleSheet, View, Text, Alert, ScrollView, TextInput, Dimensions } from 'react-native';
 import Button from "../components/Button"
+import RouteTypeRadio from "../components/RouteTypeRadio"
 import "../global.js"
 import {Location,Permissions} from 'expo';
-import RadioForm, {RadioButton, RadioButtonInput, RadioButtonLabel} from 'react-native-simple-radio-button';
- 
-class RouteTypeRadio extends React.Component {
-  constructor(props) {
-    super(props)
-    this.radio_props = [
-      {label: 'Default Route', value: 0 },
-      {label: 'Scenic Route', value: 1 }
-    ];
-    this.state = {
-      value:0
-    }
-  }
-
-  render() {
-    return (
-      <View>
-        <RadioForm
-          radio_props={this.radio_props}
-          initial={0}
-          formHorizontal={false}
-          labelHorizontal={true}
-          onPress={(value) => {this.setState({value:value})}}
-        />
-      </View>
-    );
-  }
-}
+import RadioForm from 'react-native-simple-radio-button';
 
 const STYLES = StyleSheet.create({
   text_style: {
@@ -63,15 +37,20 @@ export default class RunSetupScreen extends React.Component {
   constructor(state) {
     super(state);
     this.state = {
-      route_type : "default",
-      start: "0,0",
-      end: "0,0",
-      goal_pace: {minutes: 0, seconds: 0},
-      time: {minutes: 5, seconds: 0},
+      route_type : 0,
+      start: "-33.878363,151.104490", //Burwood
+      end: "-33.912466, 151.103120", //Campsi
+      goal_pace: {minutes: "5", seconds: "0"},
+      time: {minutes: 0, seconds: 0},
       distance: 0,
       points: 0,
+      calories: 0,
       route: null,
     }
+    this.radio_props = [
+      {label: 'Default Route', value: 0 },
+      {label: 'Scenic Route', value: 1 }
+    ];
   }
 
   calculateTime(distance, pace) {
@@ -81,6 +60,12 @@ export default class RunSetupScreen extends React.Component {
     let minutes = Math.floor(time / 60)
     let seconds = Math.floor(time - (minutes * 60))
     return {minutes:minutes, seconds:seconds}
+  }
+
+  calculateCaloriesBurnt(distance) {
+    let weight = 60
+    let calories = distance * weight * 1.036
+    return Math.floor(calories)
   }
 
   async getRouteFromAddress(start,end) {
@@ -105,11 +90,11 @@ export default class RunSetupScreen extends React.Component {
     }
   }
 
-  getRouteFromCoords(start, end) {
+  async getRouteFromCoords(start, end) {
     let url = `${global.serverURL}/api/route?start=${start}&end=${end}`;
     console.log("Getting route. URL:"+url);
     
-    fetch(url,{
+    await fetch(url,{
       method: "GET"
     })
     .catch(error => Alert.alert("Error connecting to server",error))
@@ -123,7 +108,7 @@ export default class RunSetupScreen extends React.Component {
         else {
           this.setState({
             route: res.route,
-            distance: Math.floor(res.dist/1000),
+            distance: (res.dist/1000).toFixed(3),
           });
         }
       },
@@ -131,7 +116,8 @@ export default class RunSetupScreen extends React.Component {
     );
     let points = this.calculatePoints(this.state.distance, this.state.goal_pace)
     let time = this.calculateTime(this.state.distance, this.state.goal_pace)
-    this.setState({points:points, time: time})
+    let calories = this.calculateCaloriesBurnt(this.state.distance)
+    this.setState({points:points, time: time, calories: calories})
   }
 
   setupRoute(start, end) {
@@ -155,6 +141,7 @@ export default class RunSetupScreen extends React.Component {
           <TextInput 
             style={[STYLES.text_style,STYLES.input_view]} 
             placeholder="start"
+            defaultValue={this.state.start}
             onChangeText={start => {
               this.setState({ start: start });
             }}
@@ -162,6 +149,7 @@ export default class RunSetupScreen extends React.Component {
           <TextInput 
             style={[STYLES.text_style,STYLES.input_view]} 
             placeholder="end"
+            defaultValue={this.state.end}
             onChangeText={end => {
               this.setState({ end: end });
             }}
@@ -170,10 +158,10 @@ export default class RunSetupScreen extends React.Component {
             <TextInput 
             style={STYLES.text_style} 
               placeholder="minutes"
-              defaultValue="5"
               onChangeText={minutes => {
                 this.setState({ goal_pace: {minutes: minutes} });
               }}
+              defaultValue={this.state.goal_pace.minutes}
               keyboardType="number-pad"
               returnKeyType="go"
             />
@@ -181,29 +169,40 @@ export default class RunSetupScreen extends React.Component {
             <TextInput 
               style={STYLES.text_style} 
               placeholder="seconds"
-              defaultValue="00"
               onChangeText={seconds => {
                 this.setState({ goal_pace: {seconds: seconds} });
               }}
+              defaultValue={this.state.goal_pace.seconds}
               returnKeyType="go" 
               keyboardType="number-pad"
             />
           </View>
           <Text style={[STYLES.text_style,{textAlign:"center"}]}>Select a route typing</Text>
-          <RouteTypeRadio></RouteTypeRadio>
+          <RadioForm
+            radio_props={this.radio_props}
+            initial={0}
+            formHorizontal={true}
+            labelHorizontal={true}
+            onPress={(route_type) => {this.setState({route_type:route_type})}}
+          />
+          <Button 
+            style={{borderRadius:10,padding:"2%"}} 
+            text="Generate Route Info"
+            onPress={() => {this.setupRoute(this.state.start,this.state.end)}}
+          />
         </View>
         <View style={[STYLES.container, {flex:1}]}>
           <Text style={[STYLES.text_style,{textAlign:"center"}]}>Route Information</Text>
           <Text style={STYLES.text_style}>Time: {this.state.time.minutes} minutes {this.state.time.seconds} seconds</Text>
           <Text style={STYLES.text_style}>Total Distance: {this.state.distance}km</Text>
-          <Text style={STYLES.text_style}>Points: {this.state.points} Pace-Points</Text>
+          <Text style={STYLES.text_style}>Calories Burnt/Kilojoules Burnt: {this.state.calories} Cal/ {Math.floor(this.state.calories *4.184)} Kj</Text>
+          <Text style={STYLES.text_style}>Points: {this.state.points}</Text>
         </View>
-        <View style={{padding:"3%", borderWidth:1}}>
+        <View style={STYLES.container}>
           <Button 
-            text_style={{padding:"5%"}} 
-            style={{borderRadius:10}} 
+            style={{borderRadius:10,padding:"2%"}} 
             text="Start Run"
-            onPress={() => {this.setupRoute(this.state.start,this.state.end)}}
+            onPress={() => {}}
           />
         </View>
       </View>
