283d9ac22100ad2a573e21fde69c13420059a790
Sunny Yan <sunny.yan01@gmail.com>
2019-05-23 13:54:54 +1000

    Add Google login

--- racepace/login.js
+++ racepace/login.js
@@ -4,16 +4,18 @@ import { Alert } from 'react-native';
 import Expo from 'expo';
 import io from 'socket.io-client';
 
-
 function check_login(res) {
-  if (!res) { //Check for empty response
-    Alert.alert('Error',"Server didn't respond");
+  if (!res) {
+    //Check for empty response
+    Alert.alert('Error', "Server didn't respond");
     return false;
   }
-  res_json = JSON.parse(res._bodyText); //Parse response as JSON
-  if (res_json['success']) { //Credentials correct
-    global.login_status = res_json; 
-  } else { //Credentials incorrect
+  let res_json = JSON.parse(res._bodyText); //Parse response as JSON
+  if (res_json['success']) {
+    //Credentials correct
+    global.login_status = res_json;
+  } else {
+    //Credentials incorrect
     Alert.alert('Error', res_json.error);
   }
   return res_json['success'];
@@ -21,36 +23,33 @@ function check_login(res) {
 
 function storeUserInfo(res) {
   console.log('Login success');
-  let data = {'user_id':global.login_status.user_id}
-  let info_url = global.serverURL + '/api/get_info'
-  console.log(global.login_status.token)
+  let data = { user_id: global.login_status.user_id };
+  let info_url = global.serverURL + '/api/get_info';
+  console.log(global.login_status.token);
   fetch(info_url, {
     method: 'POST',
     body: JSON.stringify(data),
     headers: new Headers({
-      'Authorization': global.login_status.token,
-    })
+      Authorization: global.login_status.token,
+    }),
   }).then(res => {
     // Store user info
-    global.user = JSON.parse(res._bodyText).info
+    global.user = JSON.parse(res._bodyText).info;
     global.socket = socket = io(
-        `${global.serverURL}?token=${global.login_status.token}`,
-        {transports: ['websocket']}
-        )
-    console.log('authenticating websocket')
-    socket.emit('authenticate', global.login_status.token)
-
-
-  })
+      `${global.serverURL}?token=${global.login_status.token}`,
+      { transports: ['websocket'] }
+    );
+    console.log('authenticating websocket');
+    socket.emit('authenticate', global.login_status.token);
+  });
 }
 
-export function login() {
+export function login(email,password) {
   //Sends login request to server
-  let data = {
-    email: this.state.email,
-    password: this.state.pword,
+  const data = {
+    email, password
   };
-  let url = global.serverURL + '/api/login';
+  const url = global.serverURL + '/api/login';
   try {
     fetch(url, {
       method: 'POST',
@@ -62,9 +61,9 @@ export function login() {
       .then(
         res => {
           console.log('Login response received from server');
-          let login_response = check_login(res)
+          let login_response = check_login(res);
           if (login_response) {
-            storeUserInfo(login_response)
+            storeUserInfo(login_response);
             this.props.navigation.navigate('FeedFollowing');
           }
         },
@@ -88,7 +87,7 @@ export function register() {
     dob: this.state.dob,
     username: this.state.username,
   };
-  let url = global.serverURL + '/api/register';
+  const url = global.serverURL + '/api/register';
   try {
     fetch(url, {
       method: 'POST',
@@ -100,9 +99,9 @@ export function register() {
       .then(
         res => {
           console.log('Login response received from server');
-          let login_response = check_login(res)
+          let login_response = check_login(res);
           if (login_response) {
-            storeUserInfo(login_response)
+            storeUserInfo(login_response);
             this.props.navigation.navigate('FeedFollowing');
           }
         },
@@ -119,13 +118,32 @@ export function register() {
 
 export async function googleLogin() {
   try {
+    const url = global.serverURL + '/api/google_login'
     const result = await Expo.Google.logInAsync({
       androidClientId: global.googleLoginID.android,
-      scopes: ['email'],
     });
     if (result.type == 'success') {
-      console.log(result);
-      //NOT IMPLEMENTED (Requires new backend code)
+      fetch(url, {
+        method: 'POST',
+        body: "idToken="+result.idToken,
+      })
+        .catch(res => {
+          Alert.alert('Error connecting to server', res);
+        })
+        .then(
+          res => {
+            console.log('Login response received from server');
+            let login_response = check_login(res);
+            if (login_response) {
+              storeUserInfo(login_response);
+              this.props.navigation.navigate('FeedFollowing');
+            }
+          },
+          reason => {
+            console.log('Promise rejected');
+            Alert.alert('Error connecting to server', reason);
+          }
+        );
     }
   } catch (err) {
     Alert.alert('Google Login Error', err);
--- racepace/screens/LoginScreen.js
+++ racepace/screens/LoginScreen.js
@@ -1,58 +1,50 @@
 import React from 'react';
 import { Component } from 'react';
-import {
-  View,
-  Text,
-  TextInput,
-  StyleSheet,
-  Image,
-  Alert,
-  Dimensions,
-} from 'react-native';
-import {login, googleLogin} from "../login"
-import Button from '../components/Button.js'
+import { View, Text, StyleSheet, Image, Alert, Dimensions } from 'react-native';
+import { login, googleLogin } from '../login';
+import Button from '../components/Button.js';
+import TextInput from '../components/TextInput';
 import '../global';
 
 const STYLES = StyleSheet.create({
   input: {
-    fontSize:20,
+    fontSize: 20,
     borderWidth: 1,
-    width:"80%",
-    borderRadius:10,
-    padding:"1%",
+    width: '80%',
+    borderRadius: 10,
+    padding: '1%',
   },
   button: {
-    width:"80%",
-    borderRadius:10,
+    width: '80%',
+    borderRadius: 10,
   },
   button_text: {
-    padding:"1%",
-    fontSize:16,
+    padding: '1%',
+    fontSize: 16,
   },
   back_btn: {
-    width:40,
-    height:"5%",
-    left:5,
-    top:20,
-    borderRadius:10,
-    alignItems: "center",
-    justifyContent: "center",
+    width: 40,
+    height: '5%',
+    left: 5,
+    top: 20,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   logo: {
-    margin:"5%",
+    margin: '5%',
     width: Dimensions.get('window').width * 0.5,
     height: Dimensions.get('window').width * 0.5,
-    borderRadius: Dimensions.get('window').width * 0.5 / 2,
+    borderRadius: (Dimensions.get('window').width * 0.5) / 2,
   },
-})
+});
 
 export default class LoginScreen extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      email: "email",
-      pword: "password",
+      email: 'email',
+      pword: 'password',
       isSigninInProgress: false,
     };
   }
@@ -61,22 +53,29 @@ export default class LoginScreen extends React.Component {
     return (
       <View>
         <Button
-            style={STYLES.back_btn}
-            text="Back"
-            text_style={STYLES.button_text}
-            onPress={()=>this.props.navigation.goBack()}
+          style={STYLES.back_btn}
+          text="Back"
+          text_style={STYLES.button_text}
+          onPress={() => this.props.navigation.goBack()}
         />
-        <View 
-          style={{alignItems:"center", flexDirection:"column",height:"95%",justifyContent: 'space-evenly'}}
-        >
-          <Image style={STYLES.logo} source={require('../assets/running.jpg')} />
+        <View
+          style={{
+            alignItems: 'center',
+            flexDirection: 'column',
+            height: '95%',
+            justifyContent: 'space-evenly',
+          }}>
+          <Image
+            style={STYLES.logo}
+            source={require('../assets/running.jpg')}
+          />
           <TextInput
             autoCorrect={false}
-            style = {STYLES.input}
-            defaultValue='email'
+            style={STYLES.input}
+            defaultValue="email"
             onChangeText={email => this.setState({ email })}
             keyboardType="email-address"
-            autoCapitalize='none'
+            autoCapitalize="none"
             returnKeyType="go"
             placeholder="Email or Mobile Num"
             placeholderTextColor="rgba(225,225,225,0.8)"
@@ -84,16 +83,26 @@ export default class LoginScreen extends React.Component {
           <TextInput
             autoCorrect={false}
             defaultValue={'password'}
-            style = {STYLES.input}
+            style={STYLES.input}
             onChangeText={pword => this.setState({ pword })}
             returnKeyType="go"
             secureTextEntry={true}
             placeholder="Password"
-            autoCapitalize='none'
+            autoCapitalize="none"
             placeholderTextColor="rgba(225,225,225,0.8)"
           />
-          <Button style={STYLES.button} text_style={STYLES.button_text} onPress={login.bind(this)} text="Login" />
-          <Button style={STYLES.button} text_style={STYLES.button_text} onPress={googleLogin} text="Login with Google" />
+          <Button
+            style={STYLES.button}
+            text_style={STYLES.button_text}
+            onPress={() => login(this.state.email, this.state.pword)}
+            text="Login"
+          />
+          <Button
+            style={STYLES.button}
+            text_style={STYLES.button_text}
+            onPress={googleLogin}
+            text="Login with Google"
+          />
         </View>
       </View>
     );
