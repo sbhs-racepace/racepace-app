7a805ed0cc4f0e3a7c713e652091aa4a5b3c806c
Jason Yu <jasonyu0100@gmail.com>
2019-06-08 10:35:41 +1000

    Removing radio buttons

--- racepace/screens/SaveRunScreen.js
+++ racepace/screens/SaveRunScreen.js
@@ -22,7 +22,6 @@ export default class SaveRunScreen extends React.Component {
   }
 
   saveRun() {
-
     let data = {
       name: this.state.name,
       description: this.state.description,
@@ -53,31 +52,79 @@ export default class SaveRunScreen extends React.Component {
     }
   }
 
+  saveRecentRun() {
+    let url = `${global.serverURL}/api/save_recent_route`
+    try {
+      fetch(url, {
+        method: 'POST',
+        headers: new Headers({
+          'Authorization': global.login_status.token,
+        })
+      })
+        .catch(res => {
+          Alert.alert('Error connecting to server', res);
+        })
+        .then(
+          res => {
+            console.log('Success Saving Recent Route');
+          },
+          reason => {
+            console.log('Promise rejected');
+            Alert.alert('Error connecting to server', reason);
+          }
+        );
+    } catch (err) {
+      Alert.alert('Error', err);
+    }
+  }
+
   render() {
+    let view;
+    if (global.current_route == null) {
+      view = (
+        <View>
+          <Text>Save Run Screen</Text>
+          <Button 
+            text="Save Run"
+            onPress={()=> {
+              Alert.alert("Saving your Run")
+              this.saveRecentRun()
+              this.props.navigation.navigate('FeedFollowing');
+            }}
+          />
+        </View>
+      );
+    } else {
+      view = (
+        <View>
+          <Text>Save Run Screen</Text>
+          <TextInput
+            onChangeText={name => {
+              this.setState({ name: name });
+            }}
+            defaultValue='Name'
+          />
+          <TextInput
+            onChangeText={description => {
+              this.setState({ description: description });
+            }}
+            defaultValue='Description'
+          />
+          <Button 
+            text="Save Run"
+            onPress={()=> {
+              Alert.alert("Saving your Run")
+              this.saveRun()
+              console.log('navigating to feed following')
+              this.props.navigation.navigate('FeedFollowing');
+            }}
+          />
+        </View>
+      );
+    }
     return (
       <View>
-        <Text>Save Run Screen</Text>
-        <TextInput
-          onChangeText={name => {
-            this.setState({ name: name });
-          }}
-          defaultValue='Name'
-        />
-        <TextInput
-          onChangeText={description => {
-            this.setState({ description: description });
-          }}
-          defaultValue='Description'
-        />
-        <Button 
-          text="Save Run"
-          onClick={()=> {
-            Alert.alert("Saving Run")
-            this.saveRun()
-            console.log('navigating to feed following')
-            this.props.navigation.navigate('FeedFollowing');
-          }}
-        />
+        {view}
       </View>
     )
   }
