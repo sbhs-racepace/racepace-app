94fb64b51735767511200f09c1ac0b0063614d81
CalvinChang1 <45642272+CalvinChang1@users.noreply.github.com>
2019-06-21 10:55:35 +1000

    Fixed styling/positioning of elements

--- screens/EditScreen.js
+++ screens/EditScreen.js
@@ -12,8 +12,8 @@ import {
   Image,
 } from 'react-native';
 import Button from '../components/Button';
-import BackButton from '../components/BackButton'
-import Color from '../constants/Color'
+import BackButton from '../components/BackButton';
+import Color from '../constants/Color';
 import '../global.js';
 
 const STYLES = StyleSheet.create({
@@ -62,13 +62,12 @@ export default class EditScreen extends React.Component {
   updateServer(username, password, profilePicture) {
     let url = global.serverURL + '/api/update_profile';
     data = {
-      username: this.state.username, 
-      password: this.state.password, 
-      full_name: this.state.full_name, 
-      bio: this.state.bio, 
+      username: this.state.username,
+      password: this.state.password,
+      full_name: this.state.full_name,
+      bio: this.state.bio,
       profile_image: this.state.profile_image,
-
-    }
+    };
     try {
       fetch(url, {
         method: 'POST',
@@ -95,79 +94,71 @@ export default class EditScreen extends React.Component {
     }
   }
 
-
-
   render() {
     return (
       <KeyboardAvoidingView
         style={{
           flex: 1,
-          backgroundColor:Color.lightBackground
-        }}
-      >
-        <BackButton
-          onPress={this.props.navigation.goBack}
-        />
-        <View
-          style={{
-            flex: 1,
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <View style={{ flex: 5, flexDirection: 'row' }}>
-            <View style={{ flex: 1 }}>
-              <Image
-                style={STYLES.profile_image}
-                source={{
-                  uri: this.state.uri,
-                }}
-              />
-              <Button
-                text="Choose File"
-                style={STYLES.chsfile}
-                onPress={async () => {
-                  let { uri, type } = await DocumentPicker.getDocumentAsync({
-                    type: 'image/*',
-                  });
-                  if (type == 'success') {
-                    this.setState({ uri });
-                  }
-                }}
-              />
-            </View>
-            <View style={{ flex: 2 }}>
-              <TextInput placeholder="Name" style={STYLES.input} />
-              <TextInput
-                placeholder="Enter Bio"
-                style={{ ...STYLES.input, height: 100, fontSize: 12 }}
-                multiline={true}
-              />
-            </View>
+          backgroundColor: Color.lightBackground,
+          justifyContent: 'space-between',
+          flexDirection: 'column',
+          alignItems: 'center',
+          
+        }}>
+        <BackButton onPress={this.props.navigation.goBack} />
+        <View style={{ flex: 1, alignSelf: 'flex-start' }} />
+        <View style={{ flex: 5, flexDirection: 'row' }}>
+          <View style={{ flex: 1 }}>
+            <Image
+              style={STYLES.profile_image}
+              source={{
+                uri: this.state.uri,
+              }}
+            />
+            <Button
+              text="Choose File"
+              style={STYLES.chsfile}
+              onPress={async () => {
+                let { uri, type } = await DocumentPicker.getDocumentAsync({
+                  type: 'image/*',
+                });
+                if (type == 'success') {
+                  this.setState({ uri });
+                }
+              }}
+            />
           </View>
-          <Text style={{ fontSize: 30 }}> Change Password</Text>
-          <TextInput placeholder="Current Password" style={STYLES.input} />
-          <TextInput placeholder="New Password" style={STYLES.input} />
-          <TextInput placeholder="Confirm New Password" style={STYLES.input} />
-          <Text style={{ fontSize: 30 }}> Change Username</Text>
-          <Text style={{ fontSize: 15 }}>
-            {' '}
-            Current Username: {global.user.username}{' '}
-          </Text>
-          <TextInput placeholder="New Username" style={STYLES.input} />
-          <View style={{ flex: 5 }}>
-          <Button
-            text="Save Changes"
-            style={{
-              width: '50%',
-              height: 30,
-              bottom: 50,
-              borderRadius: 10,
-              fontSize: 14,
-            }}
-          />
+          <View style={{ flex: 2 }}>
+            <TextInput placeholder="Name" style={STYLES.input} />
+            <TextInput
+              placeholder="Enter Bio"
+              style={{ ...STYLES.input, height: 100, fontSize: 12 }}
+              multiline={true}
+            />
           </View>
         </View>
+        <Text style={{ fontSize: 30 }}> Change Password</Text>
+        <TextInput placeholder="Current Password" style={STYLES.input} />
+        <TextInput placeholder="New Password" style={STYLES.input} />
+        <TextInput placeholder="Confirm New Password" style={STYLES.input} />
+        <Text style={{ fontSize: 30 }}> Change Username</Text>
+        <Text style={{ fontSize: 15 }}>
+          {' '}
+          Current Username: {global.user.username}{' '}
+        </Text>
+        <TextInput placeholder="New Username" style={STYLES.input} />
+        <View style={{ flex: 5 }} />
+
+        <Button
+          text="Save Changes"
+          style={{
+            width: '50%',
+            height: 30,
+            bottom: 50,
+            borderRadius: 10,
+            fontSize: 14,
+          }}
+        />
       </KeyboardAvoidingView>
     );
   }
