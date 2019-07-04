334e955f278d69f8f7c28d3e007a012db6c00981
kyb3r <abdurraqeeb53@gmail.com>
2019-07-04 12:44:29 +1000

    Use cards for route items

--- screens/RouteListScreen.js
+++ screens/RouteListScreen.js
@@ -3,24 +3,32 @@
 import React from 'react';
 import { StyleSheet, View, Text, Alert, ScrollView } from 'react-native';
 import Button from "../components/Button"
+import { Card } from 'react-native-elements'
 import "../global.js"
 import Color from '../constants/Color'
+
 
 const STYLES = StyleSheet.create({
 
   route_item: {
     padding:"3%",
-    margin:"3%",
-    borderWidth:1,
-    borderColor:"black",
+    borderWidth:5,
+    backgroundColor: Color.lightBackground,
+    borderColor: Color.darkBackground,
     borderStyle:"solid",
-    borderRadius:20,
     flexDirection:"column",
   },  
   text: {
     fontSize:15,
     padding:"3%",
     color:Color.textColor
+  },
+  card: {
+      backgroundColor: Color.lightBackground,
+      borderColor: Color.darkBackground,
+      color: Color.textColor,
+      margin: 5,
+      marginBottom: 5,
   }
 })
 
@@ -31,10 +39,13 @@ class RouteItem extends React.Component {
 
   render() {
     return (
-      <View style={STYLES.route_item}>
-        <Text style={STYLES.text}>{this.props.route.start} to {this.props.route.end}</Text>
+      <Card 
+        title={this.props.route.start + " to " + this.props.route.end}
+        titleStyle={{color: Color.textColor}}
+        dividerStyle={{display: 'none'}}
+        containerStyle={STYLES.card}>
         <Text style={STYLES.text}>{this.props.route.dist}km</Text>
-      </View>
+      </Card>
     )
   }
 }
@@ -47,7 +58,7 @@ export default class RouteListScreen extends React.Component {
   render() {
     let routes = global.user_routes.map(route => <RouteItem route={route}/>);
     return (
-      <View>
+      <View style={{backgroundColor: Color.darkBackground, padding: 10}}>
         {routes}
       </View>
     );
