0fda633545ed28e5e801909882a013da544409e0
Jason Yu <jasonyu0100@gmail.com>
2019-07-07 20:48:11 +1000

    Adding userid prop for feed item and comment feed screen as not working

--- components/FeedItem.js
+++ components/FeedItem.js
@@ -67,7 +67,7 @@ class Comment extends React.Component {
   }
 }
 
-export class FeedItem extends React.Component {
+export default class FeedItem extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -111,7 +111,7 @@ export class FeedItem extends React.Component {
           <Image
             style={STYLES.profilePic}
             source={{
-              uri: `${global.serverURL}/api/avatars/${global.login_info.user_id}.png`,
+              uri: `${global.serverURL}/api/avatars/${this.props.userid}.png`,
             }}
           />
           <View style={STYLES.user_info}>
--- screens/FeedScreen.js
+++ screens/FeedScreen.js
@@ -7,13 +7,15 @@ import { FeedItem } from '../components/FeedItem';
 import '../global.js';
 import request from '../functions/request';
 import Color from '../constants/Color';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
 let roundedButton = {
   width: '80%',
   borderRadius: 10,
 }
 
-export default class FeedScreen extends React.Component {
+class FeedScreen extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -21,89 +23,16 @@ export default class FeedScreen extends React.Component {
     };
   }
 
-  componentDidMount() {
-    
-  }
-
   render() {
-    if (!global.login_info.success && !global.TEST) {
+    if (!this.props.user.token && !global.TEST) { 
       return <Text>Please login to see your feed</Text>
-    }
-    else if (!this.state.feed) {
+    } else if (!this.state.feed) {
       let feed = request('/get_feed', 'POST', {}, true);
       this.setState({ feed: feed.feed_items });
-    }
-    else if (!global.TEST && !this.state.feed) {
+    } else if (!global.TEST && this.state.feed.length==0) {
       return (
         <View>
           <Text>Your feed is empty</Text>
-          <Button
-            style={{
-              ...global.component_styles.roundedButton,
-              alignSelf: 'center',
-            }}
-            text_style={{
-              padding: '1%',
-              fontSize: 16,
-            }}
-            onPress={() => {
-              let feed = request('/get_feed', 'POST', {}, true);
-              this.setState({ feed: feed.feed_items });
-            }}
-            text="Refresh"
-          />
-        </View>
-      );
-    }
-    return (
-      <KeyboardAvoidingView
-        behavior="position"
-        style={{ backgroundColor: Color.lightBackground }}>
-        <ScrollView
-          contentContainerStyle={{
-            backgroundColor: Color.darkBackground,
-          }}>
-          {global.TEST && (
-            <View>
-              <FeedItem
-                username="Test User"
-                posttime="3 hours ago"
-                routename="Test Route"
-                description="This is a great route"
-                length="2.2"
-                time="30"
-              />
-              <FeedItem
-                username="Test User"
-                posttime="3 hours ago"
-                routename="Test Route"
-                description="This is a great route"
-                length="2.2"
-                time="30"
-              />
-              <FeedItem
-                username="Test User"
-                posttime="3 hours ago"
-                routename="Test Route"
-                description="This is a great route"
-                length="2.2"
-                time="30"
-              />
-            </View>
-          )}
-          {this.feed &&
-            this.feed.map(item => (
-              <FeedItem
-                username={item.username}
-                posttime={item.route.real_time_route.start_time}
-                routename={item.route.real_time_route.name}
-                description={item.route.real_time_route.description}
-                length={item.route.real_time_route.route.distance}
-                likes={item.route.likes.length}
-                comments={item.route.comments}
-                routePic={item.route_image}
-              />
-            ))}
           <Button
             style={{
               ...roundedButton,
@@ -119,8 +48,94 @@ export default class FeedScreen extends React.Component {
             }}
             text="Refresh"
           />
-        </ScrollView>
-      </KeyboardAvoidingView>
-    );
+        </View>
+      );
+    } else {
+      return (<View></View>);
+    }
+    
+    // return (
+    //   <KeyboardAvoidingView
+    //     behavior="position"
+    //     style={{ backgroundColor: Color.lightBackground }}>
+    //     <ScrollView
+    //       contentContainerStyle={{
+    //         backgroundColor: Color.darkBackground,
+    //       }}
+    //     >
+    //       {global.TEST && (
+    //         <View>
+    //           <FeedItem
+    //             username="Test User"
+    //             userid="000"
+    //             posttime="3 hours ago"
+    //             routename="Test Route"
+    //             description="This is a great route"
+    //             length="2.2"
+    //             time="30"
+    //           />
+    //           <FeedItem
+    //             username="Test User"
+    //             userid="000"
+    //             posttime="3 hours ago"
+    //             routename="Test Route"
+    //             description="This is a great route"
+    //             length="2.2"
+    //             time="30"
+    //           />
+    //           <FeedItem
+    //             username="Test User"
+    //             userid="000"
+    //             posttime="3 hours ago"
+    //             routename="Test Route"
+    //             description="This is a great route"
+    //             length="2.2"
+    //             time="30"
+    //           />
+    //         </View>
+    //       )}
+    //       {this.feed &&
+    //         this.feed.map(item => (
+    //           <FeedItem
+    //             username={item.username}
+    //             userid="000"
+    //             posttime={item.route.real_time_route.start_time}
+    //             routename={item.route.real_time_route.name}
+    //             description={item.route.real_time_route.description}
+    //             length={item.route.real_time_route.route.distance}
+    //             likes={item.route.likes.length}
+    //             comments={item.route.comments}
+    //             routePic={item.route_image}
+    //           />
+    //         ))}
+    //       <Button
+    //         style={{
+    //           ...roundedButton,
+    //           alignSelf: 'center',
+    //         }}
+    //         text_style={{
+    //           padding: '1%',
+    //           fontSize: 16,
+    //         }}
+    //         onPress={() => {
+    //           let feed = request('/get_feed', 'POST', {}, true);
+    //           this.setState({ feed: feed.feed_items });
+    //         }}
+    //         text="Refresh"
+    //       />
+    //     </ScrollView>
+    //   </KeyboardAvoidingView>
+    // );
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ }, dispatch)
+}
+
+function mapStateToProps(state) {
+  const { user } = state;
+  return { user };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(FeedScreen);
