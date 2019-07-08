16e6d8a2ff6b147f50a3d4f1e20873c9680cafd4
kyb3r <abdurraqeeb53@gmail.com>
2019-07-08 11:35:14 +1000

    Add a devmode

--- global.js
+++ global.js
@@ -1,6 +1,9 @@
 // Global File is for constant variables, unlikely to change
 
+global.DEV_MODE = true // set to true if using local
+
 global.TEST =  true
 global.region = { name:"NSW Australia", coords:"-32.3010715,146.746138" }
-global.serverURL =  "https://racepace-sbhs.herokuapp.com"
-global.location_permission = false;
+global.serverURL =  global.DEV_MODE ? 'http://127.0.0.1:8000' : "https://racepace-sbhs.herokuapp.com"
+global.location_permission = false
+
