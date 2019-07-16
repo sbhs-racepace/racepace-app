c3143538a8e36fe6d4685e726785250d757c83f3
Sunny Yan <sunny.yan01@gmail.com>
2019-07-17 09:29:16 +1000

    Feed Screen WIP

--- global.js
+++ global.js
@@ -1,7 +1,6 @@
 // Global File is for constant variables, unlikely to change
 
-global.DEV_MODE = false; // set to true if using local
+global.DEV_MODE = true; // set to true if using local
 global.region = { name:"NSW Australia", coords:"-32.3010715,146.746138" }
-global.serverURL =  global.DEV_MODE ? 'http://127.0.0.1:8000' : "https://racepace-sbhs.herokuapp.com"
+global.serverURL =  global.DEV_MODE ? 'http://192.168.0.3:8000' : "https://racepace-sbhs.herokuapp.com"
 global.location_permission = false
-
--- screens/FeedScreen.js
+++ screens/FeedScreen.js
@@ -18,17 +18,44 @@ class FeedScreen extends React.Component {
     };
   }
 
+  generateFeed() {
+    return this.state.feed.map(item =>
+        {return (<FeedItem
+          username={item.user_name}
+          posttime={item.route.start_time}
+          routename={item.route.description}
+          length={item.route.estimated_distance}
+          likes={item.route.likes.length}
+          comments={item.route.comments}
+        />)}
+      )
+  }
+
   render() {
     if (!this.props.user.token == null) {
       return <Text>Please login to see your feed</Text>
-    } else if (!this.state.feed) {
-      let feed = request('/get_feed', 'POST', {}, true);
-      this.setState({ feed: feed.feed_items });
+    } else if (this.state.feed===[]) {
+      console.log("Generate feed")
+      request('/api/get_feed', 'POST', {}, this.props.user.token,
+      feed => {this.setState({ feed: feed.feed_items })})
+      return <View
+        style={{
+          backgroundColor: Color.darkBackground,
+        }}>
+        <Text>Loading...</Text>
+        </View>
     }
-    if (!this.state.feed) {
-      return (
-        <View>
-          <Text>Your feed is empty</Text>
+    console.log("Render feed")
+    return (
+      <KeyboardAvoidingView
+        behavior="position"
+        style={{ backgroundColor: Color.darkBackground }}>
+        <ScrollView
+          contentContainerStyle={{
+            backgroundColor: Color.darkBackground,
+          }}>
+          {this.generateFeed()}
+          {!this.state.feed===[] && <Text>Your feed is empty</Text>}
           <Button
             style={{
               width: '80%',
@@ -39,53 +66,13 @@ class FeedScreen extends React.Component {
               fontSize: 16,
             }}
             onPress={() => {
-              let feed = request('/get_feed', 'POST', {}, true);
-              this.setState({ feed: feed.feed_items });
+              this.setState({feed: null});
             }}
             text="Refresh"
           />
-        </View>
-      );
-    } else {
-      return (
-        <KeyboardAvoidingView
-          behavior="position"
-          style={{ backgroundColor: Color.darkBackground }}>
-          <ScrollView
-            contentContainerStyle={{
-              backgroundColor: Color.darkBackground,
-            }}>
-            {this.feed &&
-              this.feed.map(item => (
-                <FeedItem
-                  username={item.user_name}
-                  posttime={item.route.start_time}
-                  routename={item.route.description}
-                  length={item.route.estimated_distance}
-                  likes={item.route.likes.length}
-                  comments={item.route.comments}
-                />
-              ))}
-            <Button
-              style={{
-                width: '80%',
-                alignSelf: 'center',
-              }}
-              text_style={{
-                padding: '1%',
-                fontSize: 16,
-              }}
-              onPress={() => {
-                let feed = request('/get_feed', 'POST', {}, true);
-                this.setState({ feed: feed.feed_items });
-              }}
-              disabled={true}
-              text="Refresh"
-            />
-          </ScrollView>
-        </KeyboardAvoidingView>
-      );
-    }
+        </ScrollView>
+      </KeyboardAvoidingView>
+    );
   }
 }
 
