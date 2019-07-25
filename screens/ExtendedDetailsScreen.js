3d4c9799e5027d8f3fdc7e4148fbccfc8666b271
Mantank12345 <roy.wu123456789@gmail.com>
2019-07-25 21:45:17 +1000

    Fixed the text display
    Fixed the text display

--- screens/ExtendedDetailsScreen.js
+++ screens/ExtendedDetailsScreen.js
@@ -16,8 +16,15 @@ import { bindActionCreators } from 'redux';
 const { width: windowWidth, height: windowHeight } = Dimensions.get('window');
 
 const STYLES = StyleSheet.create({
-  text: {
-    fontSize: 15,
+  text1: {
+    fontSize: 40,
+    color:Color.textColor,
+    alignItems:'center',
+    textAlign: 'center',
+
+  },
+    text2: {
+    fontSize: 28,
     color:Color.textColor,
     alignItems:'center',
   },
@@ -30,19 +37,19 @@ class ExtendedDetailsScreen extends React.Component {
   }
 
   render() {
+    console.log(this.props.navigation.state.params)
     return (
       <View style={{ flex: 1 , backgroundColor:Color.lightBackground}}>
         <BackButtonHeader
           onPress={this.props.navigation.goBack}
           title='More Details'
         />
-        <ScrollView style={{ flex:1, width: '100%', alignItems:'center', justifyContent:'space-evenly'}}>
-          <Text style={STYLES.text}> Here are the details of your run: </Text>
-          <Text style={STYLES.text}> Route taken: {this.props.navigation.state.params.run.route} </Text>
-          // <Text style={STYLES.text}> Distance: {this.props.run.run_info.final_distance}km </Text>
-          // <Text style={STYLES.text}> Pace: {this.props.run.run_info.average_pace.minutes}:{this.props.run.run_info.average_pace.seconds} </Text>
-          // <Text style={STYLES.text}> Duration: Started {this.props.run.run_info.start_time}, lasting a total of {this.props.run.run_info.final_duration.minutes}:{this.props.run.run_info.final_duration.seconds} </Text>
-          <Text style={STYLES.text}>  </Text>
+        <ScrollView style={{ flex:1, width: '100%', alignItems:'center', justifyContent:'space-evenly', top: 0}}>
+          <Text style={STYLES.text1}> Here are the details of your run: </Text>
+          <Text style={STYLES.text2}> Route: from {this.props.navigation.state.params.run.run_info.start.name} to  {this.props.navigation.state.params.run.run_info.end.name} </Text>
+          <Text style={STYLES.text2}> Distance: {this.props.navigation.state.params.run.run_info.final_distance}km </Text>
+          <Text style={STYLES.text2}> Pace: {this.props.navigation.state.params.run.run_info.average_pace.minutes}:{this.props.navigation.state.params.run.run_info.average_pace.seconds} </Text>
+          <Text style={STYLES.text2}> Duration: Started {this.props.navigation.state.params.run.run_info.start_time}, lasting a total of {this.props.navigation.state.params.run.run_info.final_duration.minutes} minutes and{this.props.navigation.state.params.run.run_info.final_duration.seconds} seconds</Text>
         </ScrollView>
       </View>
     );
